0c308a9f0a41ebdd339cb1f688efe4c57745bfaf
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-24 18:03:55 +0530

    scrolling added

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,9 @@ class MyHomePage extends StatelessWidget {
         title:  Text("Transaction Manager"),
       ),
       // coloumn is a widget that takes as much width as much its children needs
-      body: Column(
+      // we need to add SingleChildScrollView to the whole column not a single file but what we can do is we can bind transaction list to a container and fix its height and then add scrolling their  
+      body: SingleChildScrollView
+      (  child: Column(
         // there are two type of allignment main axis and cross axis
         //  mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -39,7 +41,7 @@ class MyHomePage extends StatelessWidget {
           ),
         UserTransactions()
         ],
-      ),
+      ),)
     );
   }
 }
